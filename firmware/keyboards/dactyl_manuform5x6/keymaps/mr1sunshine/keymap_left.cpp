6d2d2f14bb6f84d8ad2e02698e9365ff176a4329
Aleksandr Ovchinnikov <mail@mr1sunshine.com>
2021-10-06 23:47:24 +0300

    add tap hold actions

--- firmware/keyboards/dactyl_manuform5x6/keymaps/mr1sunshine/keymap_left.cpp
+++ firmware/keyboards/dactyl_manuform5x6/keymaps/mr1sunshine/keymap_left.cpp
@@ -2,18 +2,54 @@
 
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix = {
     KEYMAP(
-        KC_ESC,  KC_1, KC_2,    KC_3,    KC_4,    KC_5,
-        KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,    KC_T,
-        KC_LCTL, KC_A, KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,    KC_B,
-                    KC_LBRC, KC_RBRC,
-                                        KC_LGUI, L_LOWER,
-                                                        KC_SPC, KC_LALT,
-                                                        KC_HOME, KC_GRAVE
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO,
+                                KC_NO, KC_NO,
+                                            KC_NO, KC_NO,
+                                            KC_NO, KC_NO
     )
 };
 
 void setupKeymap() {
+    uint32_t press[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP(
+            KC_ESC,  KC_1, KC_2,    KC_3,    KC_4,    KC_5,
+            KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,    KC_T,
+            KC_LCTL, KC_A, KC_S,    KC_D,    KC_F,    KC_G,
+            KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,    KC_B,
+                        KC_LBRC, KC_RBRC,
+                                            KC_LGUI, KC_NO,
+                                                            KC_LALT, KC_SPC,
+                                                            KC_HOME, KC_GRAVE
+    );
+
+    uint32_t tap[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP(
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                        KC_NO, KC_NO,
+                                    KC_NO, KC_SPC,
+                                                KC_NO, KC_NO,
+                                                KC_NO, KC_NO
+    );
+
+    uint32_t hold[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP(
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                        KC_NO, KC_NO,
+                                    KC_NO, L_LOWER,
+                                                KC_NO, KC_NO,
+                                                KC_NO, KC_NO
+    );
+
     uint32_t lower[MATRIX_ROWS][MATRIX_COLS] =
         KEYMAP( \
             KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, \
@@ -54,6 +90,9 @@ void setupKeymap() {
     {
         for (int col = 0; col < MATRIX_COLS; ++col)
         {
+            matrix[row][col].addActivation(_QWERTY, Method::PRESS, press[row][col]);
+            matrix[row][col].addActivation(_QWERTY, Method::MT_TAP, tap[row][col]);
+            matrix[row][col].addActivation(_QWERTY, Method::MT_HOLD, hold[row][col]);
             matrix[row][col].addActivation(_LOWER, Method::PRESS, lower[row][col]);
             matrix[row][col].addActivation(_RAISE, Method::PRESS, raise[row][col]);
             matrix[row][col].addActivation(_ADJUST, Method::PRESS, adjust[row][col]);
--- firmware/keyboards/dactyl_manuform5x6/keymaps/mr1sunshine/keymap_right.cpp
+++ firmware/keyboards/dactyl_manuform5x6/keymaps/mr1sunshine/keymap_right.cpp
@@ -2,24 +2,60 @@
 
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix = {
     KEYMAP(
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                KC_NO, KC_NO,
+                    KC_NO, KC_NO,
+        KC_NO, KC_NO,
+        KC_NO, KC_NO
+    )
+};
+
+void setupKeymap() {
+    uint32_t press[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP(
                         KC_6,    KC_7,    KC_8,     KC_9,    KC_0,      KC_MINS,
                         KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,      KC_BSPACE,
                         KC_H,    KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,
                         KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLSH,   KC_RSFT,
                                             DLTROW,  RPLACE,
-                        L_RAISE, KC_ENT,
+                        L_RAISE, KC_NO,
         KC_CAPS, KC_DEL,
         KC_TILD, KC_END
-    )
-};
+    );
 
-void setupKeymap() {
+    uint32_t tap[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP(
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                KC_NO, KC_NO,
+                    KC_NO, KC_ENT,
+        KC_NO, KC_NO,
+        KC_NO, KC_NO
+    );
+
+    uint32_t hold[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP(
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                KC_NO, KC_NO,
+                    KC_NO, KC_RSFT,
+        KC_NO, KC_NO,
+        KC_NO, KC_NO
+    );
+
     uint32_t lower[MATRIX_ROWS][MATRIX_COLS] =
         KEYMAP( \
                         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   \
                         _______, _______, _______, _______, _______, _______,  \
                         _______, _______, _______, _______, _______, _______,  \
-                        _______, _______, _______, _______, _______, _______,  \
+                        _______, _______, _______, _______, _______, KC_PIPE,  \
                                             _______, _______,                  \
                         _______, _______,                                      \
         _______, _______,                                      \
@@ -54,6 +90,9 @@ void setupKeymap() {
     {
         for (int col = 0; col < MATRIX_COLS; ++col)
         {
+            matrix[row][col].addActivation(_QWERTY, Method::PRESS, press[row][col]);
+            matrix[row][col].addActivation(_QWERTY, Method::MT_TAP, tap[row][col]);
+            matrix[row][col].addActivation(_QWERTY, Method::MT_HOLD, hold[row][col]);
             matrix[row][col].addActivation(_LOWER, Method::PRESS, lower[row][col]);
             matrix[row][col].addActivation(_RAISE, Method::PRESS, raise[row][col]);
             matrix[row][col].addActivation(_ADJUST, Method::PRESS, adjust[row][col]);
